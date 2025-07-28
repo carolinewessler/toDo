2f6f137ec4899e089b54faed93bd2a7cf4ac9efe
Caroline Wessler Fois <124738251+carolinewessler@users.noreply.github.com>
2025-07-28 19:30:55 -0300

    draggable items

--- main.js
+++ main.js
@@ -1,20 +1,62 @@
-import { Tags } from "./tags.js";
+const tags = {
+	p: document.querySelector('.p'),
+	input: document.querySelector('.input'),
+	button: document.querySelector('.button'),
+	tasks: document.querySelector('.tasks'),
+	todo: document.querySelector('.todo'),
+	inprocess: document.querySelector('.inprocess'),
+	done: document.querySelector('.done')
+}
 
-const tags = new Tags;
-document.body.append(...Object.values(tags));
+tags.button.addEventListener('click', () => { button() });
 
-tags.button.addEventListener('click', () => {
-	if (tags.input.value == ''){
-		alert('no tasks')
+tags.input.addEventListener('keydown', (e) => { if (e.key === 'Enter') button() });
+
+let draggedTask = null;
+
+function button() {
+	if (tags.input.value == '') {
+		alert('no tasks');
 	} else {
+		const taskLine = document.createElement('div');
 		const task = document.createElement('p');
-		const checkbox = document.createElement('input');
-		checkbox.setAttribute('type', 'checkbox');
+
+		taskLine.classList.add('taskline');
+		taskLine.setAttribute('draggable', 'true');
+
 		task.innerText = tags.input.value;
-
-		tags.tasks.appendChild(task);
-		tags.tasks.appendChild(checkbox);
-
+		taskLine.appendChild(task);
+		tags.todo.appendChild(taskLine);
 		tags.input.value = '';
+
+		addDragEvents(taskLine);
 	}
-})
+}
+
+function addDragEvents(element) {
+	element.addEventListener('dragstart', (e) => {
+		draggedTask = element;
+		e.dataTransfer.effectAllowed = 'move';
+	});
+
+	element.addEventListener('dragend', () => { draggedTask = null });
+}
+
+function enableDropZone(zone) {
+	zone.addEventListener('dragover', (e) => {
+		e.preventDefault();
+		e.dataTransfer.dropEffect = 'move';
+	});
+
+	zone.addEventListener('drop', () => { 
+		if (draggedTask) zone.appendChild(draggedTask);
+		zone.classList.remove('highlight'); 
+	});	
+
+	zone.addEventListener('dragenter', () => { zone.classList.add('highlight') });
+	zone.addEventListener('dragleave', () => { zone.classList.remove('highlight') });
+}
+
+enableDropZone(tags.todo);
+enableDropZone(tags.inprocess);
+enableDropZone(tags.done);
